83253656693130c6a7a316e358041b141c30eb24
Matt Parsons <parsonsmatt@gmail.com>
2016-04-02 19:05:52 -0400

    remove alias

--- src/Main.purs
+++ src/Main.purs
@@ -8,14 +8,11 @@ import Pux.Undo as Undo
 
 import Todo.UI as UI
 
-view :: Undo.History UI.State -> Html (Undo.Action UI.Action)
-view = Undo.simpleView UI.view
-
 main = do
     app <- start
             { initialState: Undo.initialState { current: "enter", todos: ["hello", "friend"] }
              , update: fromSimple (Undo.update UI.update)
-             , view: view
+             , view: Undo.simpleView UI.view
              , inputs: []
              }
 
